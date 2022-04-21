2b73c9d72f0f7fdecf74319fe8cb0e86bde1f7df
Kenneth Schultz <ken.schultz@microchip.com>
2022-04-21 14:35:11 -0700

    Added debug hooks

--- Lab7_Solution/app.X/mcc_generated_files/boot/combineAppAndBootloaderHex.sh
+++ Lab7_Solution/app.X/mcc_generated_files/boot/combineAppAndBootloaderHex.sh
@@ -1,1 +1,5 @@
+pwd
+ls ../../../
+ls ../../../boot.X/dist/default/production
 hexmate r0-3FFF,../../../boot.X/dist/default/production/boot.X.production.hex r4000-2AFFF,../../dist/default/production/app.X.production.hex r2B000-FFFFFFFF,../../../boot.X/dist/default/production/boot.X.production.hex -O../../dist/default/production/combined.production.hex
+pwd
