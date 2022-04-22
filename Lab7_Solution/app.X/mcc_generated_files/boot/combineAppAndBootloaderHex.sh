09d1cade89df18fb13f29c58f4e0f7a6ca0062b7
Kenneth Schultz <ken.schultz@microchip.com>
2022-04-22 09:07:41 -0700

    Removed debug code.

--- Lab7_Solution/app.X/mcc_generated_files/boot/combineAppAndBootloaderHex.sh
+++ Lab7_Solution/app.X/mcc_generated_files/boot/combineAppAndBootloaderHex.sh
@@ -1,8 +1,1 @@
-pwd
-ls ../../../
-ls ../../../boot.X
-ls ../../../boot.X/dist
-ls ../../../boot.X/dist/default/production
-ls ../../../app.X
 hexmate r0-3FFF,../../../boot.X/dist/default/production/boot.X.production.hex r4000-2AFFF,../../dist/default/production/app.X.production.hex r2B000-FFFFFFFF,../../../boot.X/dist/default/production/boot.X.production.hex -O../../dist/default/production/combined.production.hex
-pwd
